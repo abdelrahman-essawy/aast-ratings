d98205de3f9b495aea1a3ab2a743c4b4632c9143
Abdelrahman Essawy <boda.m7amd@gmail.com>
2023-01-24 02:45:13 +0200

    fixing import

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,30 +1,29 @@
-import { Navbar } from '@/components/Navbar/page'
 import './globals.css'
 import { Modal } from '@/components/Modal/Modal'
 import { Footer } from '@/components/Footer/page'
-import { Suspense } from 'react'
+import Navbar from '@/components/Navbar/page'
 
 export default function RootLayout({ children }) {
 
   return (
-      <html lang="en">
-        {/*
+    <html lang="en">
+      {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-        <head />
+      <head />
 
 
-        <body className='flex flex-col justify-between items-center overflow-hidden'>
-          <Modal />
+      <body className='flex flex-col justify-between items-center overflow-hidden'>
+        <Modal />
 
-          <Navbar />
-          <main className='flex-1 h-auto flex- justify-center flex-wrap w-full bg-base-200'>
-            {children}
-          </main>
-          <Footer />
-        </body>
-      </html>
+        <Navbar />
+        <main className='flex-1 h-auto flex- justify-center flex-wrap w-full bg-base-200'>
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
 
   )
 }
