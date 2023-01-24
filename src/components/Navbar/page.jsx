d4c1755ddf0d26bff101e828f55b5b3f1b2d1274
Abdelrahman Essawy <boda.m7amd@gmail.com>
2023-01-24 02:43:21 +0200

    chaniging navbar to ssr

--- src/components/Navbar/page.jsx
+++ src/components/Navbar/page.jsx
@@ -1,44 +1,17 @@
-"use client"
-import Link from 'next/link'
-import React, { lazy,  useEffect, useState } from 'react'
-import NoNavbar from './NoNavbar'
-const DesktopNavbar = lazy(() => import('./DesktopNavbar' /* webpackChunkName: "desktopNavbar" */))
-const MobileNavbar = lazy(() => import('./MobileNavbar' /* webpackChunkName: "mobileNavbar" */))
-
+import React from 'react'
+import DesktopNavbar from './DesktopNavbar'
+import MobileNavbar from './MobileNavbar'
 // eslint-disable-next-line react/display-name
-export const Navbar = () => {
-
-
-    const [width, setWidth] = useState(-10);
-
-    const setWidthBeforeRender = () => {
-        setWidth(window.innerWidth);
-        window.addEventListener('resize', () => {
-            setWidth(window.innerWidth);
-        })
-    }
-
-
-    useEffect(() => {
-        setWidthBeforeRender();
-    }, [])
-
-    const selectNavbar = (thisWidth) => {
-        if (thisWidth === -10) {
-            return <NoNavbar />
-        }
-        if (thisWidth > 768) {
-            return <DesktopNavbar />
-        } else {
-            return <MobileNavbar />
-        }
-    }
-
+const Navbar = () => {
     return (
         <div className="bg-base-200 w-full h-16">
-                {
-                    selectNavbar(width)
-                }
+            <div className='hidden sm:block'>
+                <DesktopNavbar />
+            </div>
+            <div className='block sm:hidden'>
+                <MobileNavbar />
+            </div>
         </div>
     )
 }
+export default Navbar
