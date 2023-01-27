162f63faca89939a6adca2c6ae082ec4da021035
Abdelrahman Essawy <boda.m7amd@gmail.com>
2023-01-27 15:32:04 +0200

    add fav icon

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -1,6 +1,9 @@
 export default function Head() {
   return (
     <>
+      <title>AAST - Ratings</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/preview.png" />
       <meta name="title" content="AAST - Ratings" />
       <meta name="description" content="A platform for students to share their experiences and opinions, helping to improve the quality of education. Made by Students For Students." />
       <meta name="keywords" content="AAST, Ratings, Review, Lecturers, Courses, Colleges, Departments, Computer Science, API" />
