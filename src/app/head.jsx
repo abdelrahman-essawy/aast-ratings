e8ad24ab805d740e6c595f221bdb33eadc427a38
Abdelrahman Essawy <boda.m7amd@gmail.com>
2023-01-27 15:43:43 +0200

    add favicon manifest

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -13,6 +13,11 @@ export default function Head() {
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="language" content="English" />
       <meta name="author" content="Abdelrahman Essawy" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+
       {/* <link itemprop="image" href="/preview.png" /> */}
     </>
   )
