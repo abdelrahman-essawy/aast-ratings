2b647806b4168816ae5afa9dc19ab4705b707b1a
Abdelrahman Essawy <boda.m7amd@gmail.com>
2023-01-27 15:50:53 +0200

    adding favicon.ico

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       <title>AAST - Ratings</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
       <meta property="og:image:secure_url" content="/preview.png" />
       <meta property="og:image" content="/preview.png" />
       <meta name="title" content="AAST - Ratings" />
@@ -12,9 +13,9 @@ export default function Head() {
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="language" content="English" />
       <meta name="author" content="Abdelrahman Essawy" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
       <link rel="manifest" href="/site.webmanifest" />
 
       {/* <link itemprop="image" href="/preview.png" /> */}
