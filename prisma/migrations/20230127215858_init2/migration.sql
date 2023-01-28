967ccb881bdb4c4866efc273130c321f879fc6ce
Abdelrahman Essawy <boda.m7amd@gmail.com>
2023-01-28 04:55:46 +0200

    college name not unique

--- prisma/migrations/20230127215858_init2/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name]` on the table `College` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "College_name_key" ON "College"("name");
