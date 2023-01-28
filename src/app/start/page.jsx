0238e3ab84e341fbb9fb9e2c8012374cb6bd88a0
Abdelrahman Essawy <boda.m7amd@gmail.com>
2023-01-28 03:31:47 +0200

    init

--- src/app/start/page.jsx
+++ src/app/start/page.jsx
@@ -1,36 +1,23 @@
+import { Cards } from '@/components/Cards/Cards'
+import Search from '@/components/Search/page'
+import Section from '@/utilities/StyledComponents/Section'
 import React from 'react'
-import database from '../firebase'
-import { collection, getDocs } from "firebase/firestore";
-import { Cards } from '../../components/Cards/Cards';
-import Search from '../../components/Search/page';
-import Section from '../../utilities/StyledComponents/Section';
 
 const page = () => {
-
-    const getData = async () => {
-
-        const querySnapshot = await getDocs(collection(database, "lecturers"));
-        querySnapshot.forEach((lecturer) => {
-            const { id, name, courses, ratings } = lecturer.data();
-            console.log(ratings);
-        });
-    }
-    getData()
-
     return (
         <Section>
             <div className='container py-8'>
                 <div className='flex flex-col items-center justify-center gap-8'>
                     <div className='flex flex-col items-center justify-center'>
                         <h1 className='text-3xl font-bold text-gray-200'>Computer Science</h1>
-                        <p className='text-gray-200 p-4'>Rate your lectures, improve your education.</p>
+                        <p className='text-gray-200'>Rate your lectures, improve your education. Join now!</p>
                     </div>
                     <div className='w-full'>
                         <Search />
                     </div>
-                    <section className='w-full'>
+                    <div className='w-full'>
                         <Cards />
-                    </section>
+                    </div>
                 </div>
             </div>
         </Section>
