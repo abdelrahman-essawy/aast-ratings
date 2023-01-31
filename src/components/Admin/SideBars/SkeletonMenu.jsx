0954ff924ee77e8f364ab5c6590b42f72004a40b
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-01-31 02:10:48 +0200

    overflow-x-hidden for menuskeleton

--- src/components/Admin/SideBars/SkeletonMenu.jsx
+++ src/components/Admin/SideBars/SkeletonMenu.jsx
@@ -11,7 +11,7 @@ const SkeletonMenu = ({ rows = 10 }) => {
                     All Campuses
                 </a>
 
-                <div className="flex-1 w-56 px-2 pt-2 space-y-5">
+                <div className="flex-1 w-56 px-2 pt-2 space-y-5 overflow-x-hidden">
                     {
                         [...Array(rows)].map((campus, index) => (
                             <div key={index} className='p-3'>
@@ -20,7 +20,8 @@ const SkeletonMenu = ({ rows = 10 }) => {
                         ))
                     }
                 </div>
-                <div className='flex justify-center items-center hover:bg-base-100 p-3 active:bg-base-300 transition duration-300 cursor-pointer bg-base-300 shadow border-t border-gray-600'>
+                <div
+                    className='flex sticky justify-center items-center hover:bg-base-100 p-3 active:bg-base-300 transition duration-300 cursor-pointer bg-base-300 shadow border-t border-gray-600'>
                     <AddIcon className='fill-gray-900 w-7 h-7 cursor-pointer p-1 transition duration-200 ease-in-out' />
                 </div>
             </div>
