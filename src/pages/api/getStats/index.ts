385bbad79d248b334e8cea36ad01e6e1242c7fc1
Abdelrahman Essawy <boda.m7amd@gmail.com>
2023-02-07 06:17:17 +0200

    improving getStats api endpoint

--- src/pages/api/getStats/index.ts
+++ src/pages/api/getStats/index.ts
@@ -10,49 +10,15 @@ const getStatusAPI = async (req: NextApiRequest, res: NextApiResponse) => {
         case 'GET':
             try {
 
-                let amountOfCampuses = 0
-                let amountOfColleges = 0
-                let amountOfLecturers = 0
-                let amountOfCourses = 0
-                let amountOfReviews = 0
-
-                await prisma.campus.findMany({ include: { _count: {} } })
-                    .then((campuses) => {
-                        campuses.forEach((campus) => {
-                            amountOfCampuses += 1
-                            amountOfColleges += campus._count.hasColleges
-                            amountOfReviews += campus._count.hasReviews
-                        })
-                    })
-
-                await prisma.college.findMany({ include: { _count: {}, } })
-                    .then((colleges) => {
-                        colleges.forEach((college) => {
-                            amountOfLecturers += college._count.hasLecturers
-                            // amountOfCourses += college._count.hasCourses
-                            amountOfReviews += college._count.hasReviews
-                        })
-                    })
-
-                await prisma.lecturer.findMany({ include: { _count: {} } })
-                    .then((lecturers) => {
-                        lecturers.forEach((lecturer) => { amountOfReviews += lecturer._count.hasReviews })
-                    })
-
-                await prisma.course.findMany({ include: { _count: {} } })
-                    .then((courses) => {
-                        courses.forEach((course) => {
-                            amountOfReviews += course._count.hasReviews
-                        })
-                    })
-
-
+                const amountOfCampuses = await prisma.campus.count()
+                const amountOfColleges = await prisma.college.count()
+                const amountOfLecturers = await prisma.lecturer.count()
+                const amountOfReviews = await prisma.review.count()
                 res.status(200).json(
                     {
                         Campuses: amountOfCampuses,
                         Colleges: amountOfColleges,
                         Lecturers: amountOfLecturers,
-                        //  Courses: amountOfCourses ,
                         Reviews: amountOfReviews
                     }
                 )
@@ -70,3 +36,41 @@ const getStatusAPI = async (req: NextApiRequest, res: NextApiResponse) => {
 
 }
 export default getStatusAPI
+
+
+// Legacy Code:
+                    // let amountOfCampuses = 0
+                    // let amountOfColleges = 0
+                    // let amountOfLecturers = 0
+                    // let amountOfCourses = 0
+                    // let amountOfReviews = 0
+
+                // await prisma.campus.findMany({ include: { _count: {} } })
+                //     .then((campuses) => {
+                //         campuses.forEach((campus) => {
+                //             amountOfCampuses += 1
+                //             amountOfColleges += campus._count.hasColleges
+                //             amountOfReviews += campus._count.hasReviews
+                //         })
+                //     })
+
+                // await prisma.college.findMany({ include: { _count: {}, } })
+                //     .then((colleges) => {
+                //         colleges.forEach((college) => {
+                //             amountOfLecturers += college._count.hasLecturers
+                //             // amountOfCourses += college._count.hasCourses
+                //             amountOfReviews += college._count.hasReviews
+                //         })
+                //     })
+
+                // await prisma.lecturer.findMany({ include: { _count: {} } })
+                //     .then((lecturers) => {
+                //         lecturers.forEach((lecturer) => { amountOfReviews += lecturer._count.hasReviews })
+                //     })
+
+                // await prisma.course.findMany({ include: { _count: {} } })
+                //     .then((courses) => {
+                //         courses.forEach((course) => {
+                //             amountOfReviews += course._count.hasReviews
+                //         })
+                //     })
