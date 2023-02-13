f73a9c3c5b314ed6156486f707a4c0de9faf85e3
Abdelrahman Essawy <boda.m7amd@gmail.com>
2023-02-13 04:39:27 +0200

    changing webmanifest

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -13,9 +13,14 @@ export default function Head() {
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="language" content="English" />
       <meta name="author" content="Abdelrahman Essawy" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
+      {/* <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" /> */}
+      {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" /> */}
+      {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" /> */}
+      {/* <link rel="manifest" href="/site.webmanifest" /> */}
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="manifest" href="/site.webmanifest" />
 
       {/* <link itemprop="image" href="/preview.png" /> */}
