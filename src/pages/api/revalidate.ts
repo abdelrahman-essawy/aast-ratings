739f175fb4d9af3aaa1e67bc1d7cf7aaf0dbe96f
Abdelrahman Essawy <boda.m7amd@gmail.com>
2023-02-19 21:03:24 +0200

    no cache

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,14 +1,19 @@
 export default async function handler(req, res) {
     // Check for secret to confirm this is a valid request
-    if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
-        return res.status(401).json({ message: 'Invalid token' });
-    }
+    // if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+    //     return res.status(401).json({ message: 'Invalid token' });
+    // }
+
+
 
     try {
         const { path } = req.query;
-        await res.revalidate(path);
-        return res.json({ revalidated: true });
+        
+        console.log('Revalidating: ' + path);
+        const result = await res.revalidate(path)
+        res.status(200).json('Revalidating: ' + path + result);
     } catch (err) {
-        return res.status(500).send('Error revalidating');
+        console.error(err);
+        return res.status(500).json('Error revalidating');
     }
 }
