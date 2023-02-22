c78b6d4c31e59566bc513e8e8b411052c48a73ae
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-02-22 21:28:45 +0200

    pull

--- src/components/Lecturer/Reviews.tsx
+++ src/components/Lecturer/Reviews.tsx
@@ -12,25 +12,30 @@ export const Reviews = ({ hasReviews }: { hasReviews: any }) => {
     const timeAgo = new TimeAgo('en-US')
 
     return (
-        <section id='style-1' className="bg-base-300 rounded-lg sm:p-4 p-2 overflow-y-auto max-h-80 flex-1">
+        <section id='style-1' className="bg-base-300 rounded-lg sm:p-4 p-2flex-1">
+            <div className=''>
             <h2 className="card-title mb-3 md:block">Reviews</h2>
 
-            {
+            </div>
+            <div className=' overflow-y-auto max-h-80 '>
+                {
 
-                hasReviews ? (
-                    hasReviews.map(({ id, author, comment, rating, personalSideRating, scientificSideRating, recommendationRating, createdAt, score }) => (
+                    hasReviews ? (
+                        hasReviews.map(({ id, author, comment, rating, personalSideRating, scientificSideRating, recommendationRating, createdAt, score }) => (
 
-                        <ReviewCommentTemplete key={id} id={id} author={author} comment={comment} rating={rating} personalSideRating={personalSideRating} scientificSideRating={scientificSideRating} recommendationRating={recommendationRating} createdAt={createdAt} score={score} />
+                            <ReviewCommentTemplete key={id} id={id} author={author} comment={comment} rating={rating} personalSideRating={personalSideRating} scientificSideRating={scientificSideRating} recommendationRating={recommendationRating} createdAt={createdAt} score={score} />
 
+                        )
+                        )
+
+                    ) : (
+                        <div>
+                            <h1 className="text-xl font-bold text-center">No reviews yet.</h1>
+                        </div>
                     )
-                    )
+                }
+            </div>
 
-                ) : (
-                    <div>
-                        <h1 className="text-xl font-bold text-center">No reviews yet.</h1>
-                    </div>
-                )
-            }
         </section>
     )
 }
