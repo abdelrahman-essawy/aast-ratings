af8034ab5344b139defb3fcc5c1f9c006914c6d7
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-02-27 19:40:33 +0200

    optimizing hero bg image

--- src/components/Hero/page.jsx
+++ src/components/Hero/page.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 import { Stats } from '../Stats'
 import { DesktopHero } from './DesktopHero'
@@ -6,10 +7,19 @@ import { MobileHero } from './MobileHero'
 export const Hero = () => {
 
     return (
-        <div className="hero flex-1" style={{ backgroundImage: `url("https://studentportal.aast.edu/assets/img/login-bg/10.webp")` }}>
-            <div className="hero-overlay bg-opacity-80"></div>
+        <div className="hero flex-1 relative overflow-hidden">
+            <Image
+                src="/aast-background.webp"
+                alt="hero"
+                style={{
+                    objectFit: 'cover',
+                    objectPosition: 'center',
+                }}
+                fill
+            />
+            <div className="hero-overlay bg-opacity-80 z-10"></div>
 
-            <div className="hero-content text-center text-neutral-content flex gap-8 md:gap-6 flex-col justify-between items-center h-full pb-8">
+            <div className="hero-content text-center text-neutral-content flex gap-8 md:gap-6 flex-col justify-between items-center h-full pb-8 z-20">
                 <div className='hidden sm:block'>
                     <DesktopHero />
                 </div>
